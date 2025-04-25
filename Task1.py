cc7da764a186e80ca78f759c9ae404e99d135c41
ajaybisht1903 <ajaybisht1903@gmail.com>
2025-04-25 08:07:01 +0530

    Task2

---
+++ Task1.py
@@ -0,0 +1,12 @@
+# Problem Statement: Write a Python program that does the following:
+
+a=input('Enter the first number: ')
+b=input('Enter the second number: ')
+
+a=int(a)
+b=int(b)
+
+print('Addition: ', a +b)
+print('Subtraction: ', a-b)
+print('Multiplicaton: ', a*b)
+print('Division: ', a/b)
